bd066d342bc31c8d7f53dfd70ffb638af8013870
mubaarak2002 <mubaarak.k@outlook.com>
2023-06-06 22:51:37 +0100

    testing esp32 integration with server

--- server/test.js
+++ server/test.js
@@ -3,64 +3,15 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 
-var db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "mydb"
-  });
+var http = require("http");
 
-db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    });
-
-function getHistory(vertex) {
-    // console.log("1: ", player1, " ", player2);
-
-    //query database for match history of players
-    //make sure to rearrange to player1, player2 - should not return from here alphabetically
-
-    //format data as a json object
-    //data = {"History": history} where history is a string of form 1 - 0
-    //query the rivalries table
-    //send rivalry data to socket
-    let distance;
-    let previous;
-    let data;
-
-    function get_info(vertex, callback) {
-        // console.log("2: ", player1, " ", player2);
-        sql = "SELECT dist_from_start, previous_vertex FROM dijkstra WHERE vertex = '" + vertex + "';";
-        // console.log(sql);
-        db.query(sql, function(err, results){
-            if (err){ 
-            throw err;
-            }
-            // console.log(results);
-            results.forEach((row) => {
-                distance = row.dist_from_start;
-                previous = row.previous_vertex;
-
-                // console.log("AAAAAAAA  ", P1wins, " ", P2wins);
-            });
-            return callback(results.dist_from_start);
-        });
-    }
-    
-    
-    get_info(vertex, function(result){
-        // console.log("3: ", player1, " ", player2);
-        console.log(distance);
-        console.log(previous);
-        // clientIDs.forEach(clientID => {
-        //     io.of("/client").to(clientID).emit("clientData", data);
-        // });
-        // if (gameSend) {
-        //     io.of("/webpage").to(webId).emit("history", data);
-        // }
-    });
-    return data;
+function onRequest(request, response) {
+  console.log("Received client.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hola Mundo.");
+  response.end();
 }
 
-getHistory(3);
+http.createServer(onRequest).listen(8888);
+
+console.log("Server started.");
