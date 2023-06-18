4670bac8b18eed869cd8e33de7a8868fc87997d5
longsongkk <116349817+longsongkk@users.noreply.github.com>
2023-06-18 20:47:24 +0100

    Update smooth_line.cpp

--- mapping/smooth_line.cpp
+++ mapping/smooth_line.cpp
@@ -1,85 +1,53 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-// Function to smooth and connect discontinuous line segments
-vector<vector<int>> smoothLineSegments(const vector<vector<int>>& map) {
-    vector<vector<int>> smoothedMap = map;
-
-    int rows = map.size();
-    int cols = map[0].size();
+std::vector<std::vector<int>> smoothLineSegments(const std::vector<std::vector<int>>& matrix) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    std::vector<std::vector<int>> outputMatrix(rows, std::vector<int>(cols, 0));
 
     for (int i = 0; i < rows; i++) {
+        int start_index = -1;
         for (int j = 0; j < cols; j++) {
-            if (map[i][j] == 1) {
-                // Check if the current point is disconnected from its neighbors
-                bool disconnected = false;
-
-                // Check left neighbor
-                if (j > 0 && map[i][j - 1] == 0) {
-                    disconnected = true;
+            if (matrix[i][j] == 1) {
+                if (start_index == -1) {
+                    start_index = j;
                 }
-
-                // Check right neighbor
-                if (j < cols - 1 && map[i][j + 1] == 0) {
-                    disconnected = true;
-                }
-
-                // Check top neighbor
-                if (i > 0 && map[i - 1][j] == 0) {
-                    disconnected = true;
-                }
-
-                // Check bottom neighbor
-                if (i < rows - 1 && map[i + 1][j] == 0) {
-                    disconnected = true;
-                }
-
-                // If the current point is disconnected, connect it to the neighbors
-                if (disconnected) {
-                    // Check left neighbor
-                    if (j > 0 && map[i][j - 1] == 0) {
-                        smoothedMap[i][j - 1] = 1;
+            } else {
+                if (start_index != -1) {
+                    int end_index = j - 1;
+                    for (int k = start_index; k <= end_index; k++) {
+                        outputMatrix[i][k] = 1;
                     }
-
-                    // Check right neighbor
-                    if (j < cols - 1 && map[i][j + 1] == 0) {
-                        smoothedMap[i][j + 1] = 1;
-                    }
-
-                    // Check top neighbor
-                    if (i > 0 && map[i - 1][j] == 0) {
-                        smoothedMap[i - 1][j] = 1;
-                    }
-
-                    // Check bottom neighbor
-                    if (i < rows - 1 && map[i + 1][j] == 0) {
-                        smoothedMap[i + 1][j] = 1;
-                    }
+                    start_index = -1;
                 }
+            }
+        }
+
+        if (start_index != -1) {
+            int end_index = cols - 1;
+            for (int k = start_index; k <= end_index; k++) {
+                outputMatrix[i][k] = 1;
             }
         }
     }
 
-    return smoothedMap;
+    return outputMatrix;
 }
 
 int main() {
-    // Sample input map
-    vector<vector<int>> map = {
-       //here input the image from  vision processing
-    };
+    //input matrix here
+    
 
-    // Smooth and connect discontinuous line segments
-    vector<vector<int>> smoothedMap = smoothLineSegments(map);
+    // Call the function
+    std::vector<std::vector<int>> outputMatrix = smoothLineSegments(inputMatrix);
 
-    // Print the smoothed map
-    for (const auto& row : smoothedMap) {
-        for (const auto& val : row) {
-            cout << val << " ";
+    // Print the output
+    for (const auto& row : outputMatrix) {
+        for (int value : row) {
+            std::cout << value << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 
     return 0;
