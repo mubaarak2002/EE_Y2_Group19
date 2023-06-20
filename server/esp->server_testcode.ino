5c7aed9318e72ab8bd43b7754531f6ed20a7bd32
mubaarak2002 <mubaarak.k@outlook.com>
2023-06-20 13:28:44 +0100

    fpga to esp code

--- server/esp->server_testcode.ino
+++ server/esp->server_testcode.ino
@@ -3,11 +3,18 @@
 #include <WebSocketClient.h>
 
 // wifi connection
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "AndroidAP";
+const char* password = "mubs2002";
 char path[] = "/";
 // aws server public ip (port is always 5000)
-char host[] = "54.84.92.150:5000";
+char host[] = "3.91.176.252:5000";
+
+// FPGA setup
+const int bufferSize = 40;  // Size of the buffer to store the image, corresponds to message length from fpga
+uint8_t imageBuffer[bufferSize];
+int bytesRead = 0;  // Number of bytes read
+int UART_offset = 0;
+int shiftedData [20];
   
 WebSocketClient webSocketClient;
 
@@ -41,7 +48,7 @@ void setup() {
   
 
   // Connect to the websocket server
-  if (client.connect("54.84.92.150", 5000)) {
+  if (client.connect("3.91.176.252", 5000)) {
     Serial.println("Connected");
   } else {
     Serial.println("Connection failed.");
@@ -64,8 +71,6 @@ void setup() {
 
 }
 
-// this function puts the coordinates in the correct format to send to the server
-// 2 variables - 2D array of wall coordinates - 1D array of rover coordinates
 String ToServer(int coordinates[ 9 ][ 2 ], int rovcoordinates[ 2 ]){
   String data;
   String rcv;
@@ -84,37 +89,85 @@ String ToServer(int coordinates[ 9 ][ 2 ], int rovcoordinates[ 2 ]){
   
 }
 
+void FPGA_vars(){
+  if (Serial.available()) {
+    // Read the incoming data and store it in the buffer
+    bytesRead += Serial.readBytesUntil('\n', imageBuffer, bufferSize);
+    
+    
+    // Check if the entire image has been received
+    if (bytesRead >= bufferSize) {
+      // Image received completely
+      
+      // Process the image data as needed
+      // You can save it to an SD card, display it on a screen, or perform any other operation
+      
+      // Reset the bytesRead counter for the next image
+      for (int i = 0; i < bufferSize; i++){
+        //Serial.println(i);
+//        Serial.print(imageBuffer[i]);
+//        Serial.print(i);
+//        Serial.print(" ");
+        if (imageBuffer[i+1] == 66){
+          if (imageBuffer[i+2] == 66){
+            if (imageBuffer[i+3] == 82){
+              UART_offset = i;
+//              Serial.println();
+//              Serial.println(UART_offset);
+            }
+          }
+        }
+        shiftedData[i-UART_offset] = imageBuffer[i+1];
+//        Serial.print(shiftedData[i]);
+//        Serial.print(" ");
+      }
+      Serial.print("stream in:");
+      for(int i = 0; i < 40; i++){
+        Serial.print(imageBuffer[i]);
+        Serial.print(" ");
+      }
+      Serial.println();
+
+      Serial.print("shifted::");
+      for(int i = 0; i < 20; i++){
+        Serial.print(shiftedData[i]);
+        Serial.print(" ");
+      }
+      Serial.println();
+      bytesRead = 0;
+    }
+  UART_offset = 0;
+  }
+}
+
 void loop() {
+  FPGA_vars();
   String data;
   String rcv;
 
   if (client.connected()) {
     
     webSocketClient.getData(rcv);
-    if (data.length() > 0) {
-      Serial.print("Received data: ");
-      Serial.println(rcv);
+    if (rcv.length() > 0) {
+      //Serial.print("Received data: ");
+      //Serial.println(rcv);
     }
 
     int coordinates[ 9 ][ 2 ] = {
-      { 1, 1 },
-      { 2, 1 },
-      { 3, 1 },
-      { 4, 1 },
-      { 5, 1 },
-      { 6, 1 },
-      { 7, 1 },
-      { 8, 1 },
-      { 9, 1 },
+      { shiftedData[0] , shiftedData[1] },
+      { shiftedData[2] , shiftedData[3] },
+      { shiftedData[4] , shiftedData[5] },
+      { shiftedData[6] , shiftedData[7] },
+      { shiftedData[8] , shiftedData[9] },
+      { shiftedData[10] , shiftedData[11] },
+      { shiftedData[12] , shiftedData[13] },
+      { shiftedData[14] , shiftedData[15] },
+      { shiftedData[16] , shiftedData[17] },
     };
 
-    int rovcoordinates[ 2 ] = { 100, 200 };
+    int rovcoordinates[ 2 ] = { shiftedData[18], shiftedData[19] };
 
-    Serial.print("in");
     data = ToServer(coordinates, rovcoordinates);
-    Serial.print("\n");
-    Serial.print("out");
-    Serial.print("\n");
     webSocketClient.sendData(data);
 
   } else {
